f66333b9d68f3143aea723c2be7715b6dcb01245
Jan Rafael Guno <guno.148983@pasay-edsa.sti.edu.ph>
2022-08-01 19:59:36 +0800

    New

--- src/BlogList.js
+++ src/BlogList.js
@@ -5,8 +5,8 @@ const BlogList = ({blogs,title}) => {
         <div className="blog-list">
             <h1>{title}</h1>
            {
-                blogs.map((b) => (
-                    <div key={b.id} className="blog-preview">
+                blogs.map((b) => (                 
+                <div key={b.id} className="blog-preview">
                         <Link to={`/blogs/${b.id}`}>
                             <h2>{b.title}</h2>
                             <p>Written by { b.author }</p>
